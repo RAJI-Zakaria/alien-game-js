bcb4c3cb3ff347260d2d5b311d6a8b5d5c826b0b
RAJI-Zakaria <zakariaraji.me@gmail.com>
2021-11-20 22:29:31 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 500;
+
+
+const Keys = [];
+
+const player = {
+    x:0,
+    y:0,
+    width:??,
+    height:??,
+    frameX:,
+    frameY:,
+    speed 9,
+    moving: false
+};
+
+const player
+
