3b978490e3dc210ba959eb5c9098294db1fc9738
RAJI-Zakaria <zakariaraji.me@gmail.com>
2021-11-21 21:06:28 +0100

    adding functions

--- js/app.js
+++ js/app.js
@@ -4,19 +4,52 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
 
-
 const Keys = [];
 
 const player = {
     x:0,
     y:0,
-    width:??,
-    height:??,
-    frameX:,
-    frameY:,
-    speed 9,
+    width:32,
+    height:54,
+    frameX:0,
+    frameY:0,
+    speed: 9,
     moving: false
 };
 
-const player
 
+
+const playerSprite = new Image();
+playerSprite.src= "assets/characters/aladdin_32x54.png";
+
+const background = new Image();
+background.src= "assets/characters/background.png";
+
+function drawSprite(img, sX, SY, sW, sH, dX, dY, dW, dH){
+    ctx.drawImage(img, sX, SY, sW, sH, dX, dY, dW, dH);
+}
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.drawImage(background,0,0,canvas.width,canvas.height);
+    drawSprite(playerSprite, 0, 0, player.width, player.height, 0,0, player.width, player.height);
+    movePlayer();
+    requestAnimationFrame(animate);
+     
+}
+animate();
+
+window.addEventListener("keydown", function(e){
+    Keys[e.key] = true;
+});
+
+window.addEventListener("keyup", function(e){
+    delete Keys[e.key];
+});
+
+function movePlayer(){
+    if(Keys['ArrowUp']){
+        player.y +=player.speed;
+        player.frameY=3;
+    }
+     
+}
